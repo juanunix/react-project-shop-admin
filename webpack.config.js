f628641f18e140699d088ef8bbe1f18d19a1d1cf
Vikram Jadhav <vikramcse.10@gmail.com>
2016-04-04 14:21:54 +0530

    modified webpack conf file for react production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+require('argv-set-env')()
+
 var path = require('path');
+var webpack = require('webpack')
 
 var config = {
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-source-map',
   entry: path.join(__dirname, 'app/index.js'),
   output: {
     path: path.join(__dirname, '/build/'),
@@ -12,13 +15,19 @@ var config = {
     loaders: [{
       test: /\.js?$/,
       loader: 'babel-loader',
-      exclude: /(node_modules|bower_components)/,
+      exclude: /node_modules/,
       query: {
         presets: ['es2015', 'react']
       }
     }]
   },
-
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
 };
 
 module.exports = config;
